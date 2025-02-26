87863a235a8d27ecd724fad275b29c80bba03681
Anastasiia Pryimak <aquaprint.it.department@gmail.com>
2025-02-26 12:02:22 +0200

    Added google sheets api

--- js/sheets-request.js
+++ js/sheets-request.js
@@ -5,7 +5,7 @@ document.getElementById("sendData").addEventListener("click", function () {
         message: "Привіт, це тест!"
     };
 
-    fetch("AKfycbwZF5p8yudA9elKI3_-PvKtbMq4vqtccSov4Ux-dYehpGpIgakp1kFZMYqBakeprhFK", {
+    fetch("https://script.google.com/macros/s/AKfycbwZF5p8yudA9elKI3_-PvKtbMq4vqtccSov4Ux-dYehpGpIgakp1kFZMYqBakeprhFK/exec", {
         method: "POST",
         mode: "no-cors",
         headers: {
